7fe96a90c78074b4bea5af0046c4ca2d5c6e6801
Daria Supińska <dariasupinska@gmail.com>
2021-02-07 19:19:13 +0100

    product-list component adjusted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>`
 })
 export class AppComponent {
   pageTitle: string = 'Product Management App';
