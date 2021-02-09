c808ca4ce60ff30e8c4c27dff3b683255365b4f3
Daria Supińska <dariasupinska@gmail.com>
2021-02-09 20:43:53 +0100

    subscripe added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,39 +12,43 @@ export class ProductListComponent implements OnInit {
     imageWidth: number =50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
     
-    _listFilter: string;
+    _listFilter = '';
     get listFilter(): string {
-        return this._listFilter;
+      return this._listFilter;
     }
 
     set listFilter(value: string) {
         this._listFilter = value;
-        this.filteredProducts=this.listFilter ? this.performFilter(this.listFilter) : this.products;
-    }
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+      }
 
-    filteredProducts: IProduct[];
-    products: IProduct[] = [];
+      filteredProducts: IProduct[] = [];
+      products: IProduct[] = [];
 
-    constructor(private productService: ProductService) {
-    
-    }
+      constructor(private productService: ProductService) { }
 
-    onRatingClicked(message: string): void {
+      onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
-    }
+      }
 
-    performFilter(filterBy: string): IProduct[] {
+      performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
-            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
+          product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+      }
 
-    toggleImage(): void {
-        this.showImage=!this.showImage;
+      toggleImage(): void {
+        this.showImage = !this.showImage;
+      }
+      ngOnInit(): void {
+        this.productService.getProducts().subscribe({
+          next: products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+          },
+          error: err => this.errorMessage = err
+        });
+      }
     }
-    ngOnInit(): void {
-        this.products=this.productService.getProducts();
-        this.filteredProducts = this.products;
-    }
-}
