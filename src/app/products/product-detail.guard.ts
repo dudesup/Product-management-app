81513465f50d90165c4b214770e245948bab6e91
Daria Supińska <dariasupinska@gmail.com>
2021-02-14 23:29:34 +0100

    product component adjusted

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -7,18 +7,17 @@ import { Observable } from 'rxjs';
 })
 export class ProductDetailGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      let id = +next.url[1].path;
-      if (isNaN(id) || id < 1 ) {
-        alert("Invalid product id");
-        this.router.navigate(['/products']);
-        return false;
-      };
+    const id = +next.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert('Invalid product Id');
+      this.router.navigate(['/products']);
+      return false;
+    }
     return true;
   }
-  
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -6,31 +6,36 @@ import { catchError, tap, map } from 'rxjs/operators';
 import { IProduct } from './product';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProductService {
+  private productUrl = 'assets/products/products.json';
 
-    private productUrl = 'assets/products/products.json';
-    
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
 
-    getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl)
-          .pipe(
-            tap(data => console.log('All: ' + JSON.stringify(data))),
-            catchError(this.handleError)
-          );
-      }
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
 
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+      .pipe(
+        map((products: IProduct[]) => products.find(p => p.productId === id))
+      );
+  }
 
-    private handleError(err: HttpErrorResponse): Observable<never> {
-        let errorMessage = '';
-        if (err.error instanceof ErrorEvent) {
-          errorMessage = `An error occurred: ${err.error.message}`;
-        } else {
-          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-        }
-        console.error(errorMessage);
-        return throwError(errorMessage);
-      }
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
     }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
